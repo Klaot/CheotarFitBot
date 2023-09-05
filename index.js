4a211fa4837d04240ffa37f6898eb9e3f47d1096
Vitalya <klaot@yandex.ru>
2023-09-05 10:26:50 +0200

    text

--- index.js
+++ index.js
@@ -57,6 +57,8 @@ const welcomeMessage = `Привет!👋
 app.post(`/webhook/${token}`, (req, res) => {
   const { message } = req.body;
 
+  console.log("message", message);
+
   if (message && message.text) {
     const chatId = message.chat.id;
     const text = message.text;
